f5926fa36b6b100234c2a40cf12ec8f24588dd10
yunakr <kya7gogo@naver.com>
2023-05-29 21:52:19 +0900

    윤아반응형수정

--- src/collection.js
+++ src/collection.js
@@ -9,15 +9,18 @@ const actionBarName = "컬렉션";
 /*사진 배열*/
 const SquareGrid = () => {
   const squares = [
-    {id:1, color:'gold'},    
-    {id:2, color:'gold'},
-    {id:3, color:'gold'},
-    {id:4, color:'gold'},
-    {id:5, color:'gold'},
-    {id:6, color:'gold'},
-    {id:7, color:'gold'},
-    {id:8, color:'gold'},
-    {id:9, color:'gold'},
+    {id:1, color:'#efb82c'},    
+    {id:2, color:'#efb82c'},
+    {id:3, color:'#efb82c'},
+    {id:4, color:'#efb82c'},
+    {id:5, color:'#efb82c'},
+    {id:6, color:'#efb82c'},
+    {id:7, color:'#efb82c'},
+    {id:8, color:'#efb82c'},
+    {id:9, color:'#efb82c'},
+    {id:9, color:'#efb82c'},
+    {id:9, color:'#efb82c'},
+
   ];
 
   /*사진이 한 줄에 3개 이상이면 다음 줄에 뜨게 해줌*/
@@ -33,26 +36,16 @@ const SquareGrid = () => {
   /*현재는 한 줄에 사진 3개까지만 보이게 해둠*/
   const chunkedSquares = chunk(squares, 3);
 
-  return (
-    <div className="squareBox">
+  return ( 
+    <div className="flex flex-wrap grid gap-3 grid-cols-3 ml-4  justify-center">
       {chunkedSquares.map((row, rowIndex) => (
-        <div key={rowIndex}>
+        <div key={rowIndex} className="flex flex-wrap justify-start ">
           {row.map((square) => (
-            <div
-              key={square.id}
-              style={{
-                margin: '19px', /*전체 여백 맞추기*/
-                marginTop: '10px',
-                marginRight: '10px',
-                marginLeft: '28px', /*양쪽 여백 차이 맞추기*/
-                display: 'inline-block',
-                width: '82px',
-                height: '82px',
-                borderRadius: '13px', /*사각형 둥글게*/
-                backgroundColor: square.color,
-                boxShadow: '0 0 5px rgba(0,0,0,0.3)' /*박스 그림자*/
-              }}
-            ></div>
+              <button
+                key={square.id}
+                className=  
+                "m-2 mt-7 inline-block w-20 h-20 rounded-lg bg-yellow-500 shadow-md"
+              ></button>
           ))}
         </div>
       ))}
@@ -60,29 +53,15 @@ const SquareGrid = () => {
   );
 };
 
-const CircleBtn = () => {
-  const handleClick = () => {
-    console.log("클릭");
-  };
-
-  return (
-    <div className="btn">
-    <button className="circleBtn" 
-            onClick={handleClick} 
-            style={{fontSize: '60px'}}>
-    +
-    </button>
-    </div>
-  )
-};
-
+<div className="flex m-8">
+  <button img src='img\plusButton.png'></button>
+</div>
 
 function Collection() {
   return (
     <div>
       <ActionBar actionBarName={actionBarName} />
       <SquareGrid/>
-      <CircleBtn/>
       <Nav/>
     </div>
   );
