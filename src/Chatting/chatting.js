c5b2a703205579292423f737329483b3372918a3
신유진 <95026300+yudaeng@users.noreply.github.com>
2023-08-11 03:49:32 +0900

    ㅠㅠ..

--- src/Chatting/chatting.js
+++ src/Chatting/chatting.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Nav } from '../Component/Nav';
 import { ActionBar } from '../Component/ActionBar';
-
+import { Link } from 'react-router-dom';
 // 액션바 이름
 const actionBarName = "채팅목록";
 
@@ -9,14 +9,16 @@ const Chatting = () => {
   /* 값을 받아올 변수들 */
   const chat_id = '이름';
   const chat_explain = '안녕하세요';
-  const date_month = '0';
-  const date_day = '0';
+
 
   return (
     <>
       <ActionBar actionBarName={actionBarName} />
       {/* 채팅목록 */}
+
+      <Link to="/chatdetails">
       <button className="w-full">
+
         <div className="p-5 flex border">
           <div>
             <img src="img/item_gray.png" alt="프로필사진" className="rounded-full" style={{ width: 60 }}></img>
@@ -30,13 +32,11 @@ const Chatting = () => {
               {chat_explain}
             </div>
           </div>
-
-          <div style={{ fontSize: '0.7rem' }} className="ml-auto">
-            {date_month}월{date_day}일
-          </div>
+      
         </div>
-      </button>
 
+      </button>
+      </Link>
       {/* 추가 채팅버튼(예시) */}
       <button className="w-full">
         <div className="p-5 flex border">
@@ -53,9 +53,7 @@ const Chatting = () => {
             </div>
           </div>
 
-          <div style={{ fontSize: '0.7rem' }} className="ml-auto">
-            {date_month}월{date_day}일
-          </div>
+      
         </div>
       </button>
 
